4fb1ebbd7b84ba1a0070594798c6ccc7a64af38a
Ben Payne <trixtur@gmail.com>
2021-03-20 23:30:40 -0600

    Added FingerOfDeath Spell.

---
+++ CsBot.Tests/WavingHands/FingerOfDeathTest.cs
@@ -0,0 +1,37 @@
+using CsBot.Games.WavingHands;
+using CsBot.Games.WavingHands.Living;
+using CsBot.Games.WavingHands.Spells;
+using NUnit.Framework;
+
+namespace CsBot.Tests.WavingHands
+{
+	[TestFixture]
+	public class FingerOfDeathTest
+	{
+		[Test]
+		public void TestFingerOfDeathLeft ()
+		{
+			var hand = new Hand (Hand.Side.Left);
+			var hand2 = new Hand (Hand.Side.Right);
+			var wizard = new Wizard ();
+
+			Hand.Side s = hand.GetSide ();
+			Assert.AreEqual (s, Hand.Side.Left);
+			Hand.Side s2 = hand2.GetSide ();
+			Assert.AreEqual (s2, Hand.Side.Right);
+
+			hand.Add (Gesture.ProfferedPalm);
+			hand.Add (Gesture.Wave);
+			hand.Add (Gesture.ProfferedPalm);
+			hand.Add (Gesture.WigglingFingers);
+			hand.Add (Gesture.Snap);
+			hand.Add (Gesture.Snap);
+			hand.Add (Gesture.Snap);
+			hand.Add (Gesture.DigitPoint);
+
+			Spell fingerOfDeath = new FingerOfDeath (wizard);
+
+			Assert.True (fingerOfDeath.IsMatch (hand, hand2));
+		}
+	}
+}
