30511476a3b83deeb8bf3831546a76a654d996cb
Ben Payne <trixtur@gmail.com>
2021-03-24 21:06:09 -0600

    Added Cure Heavy Wounds with test and completed Protection Spells.

---
+++ CsBot.Tests/WavingHands/CureHeavyWoudsTest.cs
@@ -0,0 +1,26 @@
+using CsBot.Games.WavingHands;
+using CsBot.Games.WavingHands.Spells;
+using NUnit.Framework;
+
+namespace CsBot.Tests.WavingHands
+{
+	[TestFixture]
+	public class CureHeavyWoudsTest : TestSpell
+	{
+		[Test]
+		public void TestCureHeavyWounds ()
+		{
+			MakeTestObjects ();
+
+			TestHands ();
+
+			hand.Add (Gesture.DigitPoint);
+			hand.Add (Gesture.WigglingFingers);
+			hand.Add (Gesture.ProfferedPalm);
+			hand.Add (Gesture.Wave);
+
+			Spell cureHeavyWounds = new CureHeavyWounds (wizard);
+			Assert.True (cureHeavyWounds.IsMatch (hand, hand2));
+		}
+	}
+}
--- src/CsBot/Games/WavingHands/Gesture.cs
+++ src/CsBot/Games/WavingHands/Gesture.cs
@@ -2,13 +2,21 @@ namespace CsBot.Games.WavingHands
 {
 	public enum Gesture : uint
 	{
+		/// <summary>Blank represents a space</summary>
 		Blank		= ' ',
+		/// <summary>Null (\0)</summary>
 		Null		= '\0',
+		/// <summary>ProfferedPam (P)</summary>
 		ProfferedPalm	= 'P',
+		/// <summary>WigglingFingers (F)</summary>
 		WigglingFingers = 'F',
+		/// <summary>Snap (S)</summary>
 		Snap		= 'S',
+		/// <summary>Wave (W)</summary>
 		Wave		= 'W',
+		/// <summary>DigitPoint (D)</summary>
 		DigitPoint	= 'D',
+		/// <summary>Clap (C)</summary>
 		Clap		= 'C', // Requires both hands
 	}
 }
